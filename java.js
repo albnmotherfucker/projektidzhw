bd0a635c43bce105410a99938a8d66440a104eb3
Rion Makolli <makollirion@gmail.com>
2023-12-07 00:19:18 +0100

    butonat jane te ndrequr

--- java.js
+++ java.js
@@ -2,7 +2,21 @@ window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
     var header = document.getElementById("myHeader");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.style.backgroundColor = "black"; // Change this to the desired color
+    var buttons = document.querySelectorAll(".header button");
+
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        header.style.backgroundColor = "black";
+        buttons.forEach(function(button) {
+            button.style.backgroundColor = "#303030";
+            button.style.color= "white";
+            
+        });
     } else {
-        header.style.backgroundColor = "transparent"; }}
+        header.style.backgroundColor = "transparent";
+        buttons.forEach(function(button) {
+            button.style.backgroundColor = "transparent";
+            button.style.color= "white";
+            
+        });
+    }
+}
